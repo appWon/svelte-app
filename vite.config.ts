79ced5efb33f63bb2a1ee87c17ef58b0d60b85ed
LioFrontend <lio@waynehills.co>
2022-03-21 01:48:05 +0900

    refact - env 포트

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import path from 'path';
 import 'dotenv/config';
 
 export default defineConfig(({ mode }: ConfigEnv) => {
+    const port = mode === 'development' && 3001;
+
     const server = {
-        port: 3001,
+        port,
         proxy: {
             '/ttv/api': {
                 target: process.env.BACK_END_URL,
