e3a0d214a223fb1130eb5e2fab265454682c8641
LioFrontend <lio@waynehills.co>
2022-03-20 22:24:28 +0900

    style - color 세팅

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,31 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        colors: {
+            'brand-main-color': ' #650aa8',
+            'brand-sub-color': ' #bb99fc',
+            'brand-sub2-color': ' #7450ff',
+            'brand-sub3-color': ' #e2e2ff',
+
+            // Text color
+            'text-main-color': ' #ffffff',
+            'text-sub-color': ' #aaaaaa',
+            'text-sub2-color': ' #d8d8d8',
+
+            // Background color
+            'bg-white-color': ' #ffffff',
+            'bg-black-color': ' #111111',
+            'bg-gray1-color': ' #222222',
+            'bg-gray2-color': ' #333333',
+            'bg-gray3-color': ' #444444',
+            'bg-gray4-color': ' #555555',
+            'bg-disabled-color': ' #767676',
+
+            // etc
+            'disabled-color': ' #444444',
+            'warning-color': ' #ff5f0a',
+            'divider-color': ' #3c3c3c',
+        },
+    },
+    plugins: [],
 };
