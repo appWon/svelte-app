b03762d00ade78b8cf5f9dc983b4a47b978b0075
LioFrontend <lio@waynehills.co>
2022-03-21 01:41:32 +0900

    feat - env

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import { ConfigEnv, defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
+import { ConfigEnv, defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+import 'dotenv/config';
 
 export default defineConfig(({ mode }: ConfigEnv) => {
-  const server = {
-    port: 3001,
-    proxy: {
-      "/ttv/api": {
-        target: "http://app235.iptime.org:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  };
+    const server = {
+        port: 3001,
+        proxy: {
+            '/ttv/api': {
+                target: process.env.BACK_END_URL,
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    };
 
-  return {
-    plugins: [svelte()],
-    resolve: {
-      alias: {
-        "@/lib": path.resolve(__dirname, "src/lib"),
-        "@/pages": path.resolve(__dirname, "src/pages"),
-      },
-    },
-    server,
-  };
+    return {
+        plugins: [svelte()],
+        resolve: {
+            alias: {
+                '@/lib': path.resolve(__dirname, 'src/lib'),
+                '@/pages': path.resolve(__dirname, 'src/pages'),
+            },
+        },
+        server,
+    };
 });
